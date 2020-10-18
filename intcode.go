d636dcf83a15aa9a1507197f8fb9192e346d8cc8
Fanny Rottee <frottee@vivlio.com>
2020-10-18 17:45:56 +0200

    add input and output instructions

--- intcode.go
+++ intcode.go
@@ -6,12 +6,23 @@ import (
 )
 
 func computeIntcode(input []int) ([]int, error) {
-	for i := 0; i < len(input); i += 4 {
+	for i := 0; i < len(input); {
 		switch input[i] {
 		case 1:
 			input[input[i+3]] = input[input[i+1]] + input[input[i+2]]
+			i += 4
 		case 2:
 			input[input[i+3]] = input[input[i+1]] * input[input[i+2]]
+			i += 4
+		case 3:
+			var first int
+			fmt.Print("Input: ")
+			fmt.Scanln(&first)
+			input[input[i+1]] = first
+			i += 2
+		case 4:
+			fmt.Println(fmt.Sprintf("Output: %d", input[input[i+1]]))
+			i += 2
 		case 99:
 			return input, nil
 		default:
